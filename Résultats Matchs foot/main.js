bbffb17e20a747c9e038ea8babba16ae47a77552
EmmanuelD69 <emmanueldurouge@yahoo.com>
2019-08-04 19:26:42 +0200

    Update main.js

--- Résultats Matchs foot/main.js
+++ Résultats Matchs foot/main.js
@@ -7,7 +7,10 @@ var equipe2 = prompt("Entrez le nom de la deuxième équipe:")
 // Affichage dans la fenêtre web du texte de présentation des équipes qui vont s'affronter
 document.write("Aujourd'hui, " + equipe1 + " affronte " + equipe2 + "<br>")
 
-// Création de deux variables qui vont se voir attribué chacune, à tour de role, un nombre aléatoire entre 0 et 5
+// Création de deux variables qui vont se voir attribué chacune, à tour de role, un nombre aléatoire entre 0 et 5.
+// La fonction Math.floor(x) renvoie le plus grand entier qui est inférieur ou égal à un nombre.
+/* La fonction Math.random() renvoie un nombre flottant pseudo-aléatoire compris dans l'intervalle [0, 1[ (ce qui signifie que 0 est compris dans l'intervalle mais que 1 en est exclu) selon une distribution approximativement uniforme sur cet intervalle. Ce nombre peut ensuite être multiplié afin de couvrir un autre intervalle.*/
+// Nous aurons donc ici un nombre aléatoire compris entre 0 et 6 mais dont le nombre 6 est exclu.
 var score1 = Math.floor(Math.random() * 6);
 console.log(score1)
 var score2 = Math.floor(Math.random() * 6);
