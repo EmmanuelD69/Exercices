87ee576a984130666d3c3f362bb246ebb1d1fe54
damienmarchandfr <damien-marchand@outlook.fr>
2019-08-05 21:39:19 +0200

    propositions

--- Affiche TTC/main.js
+++ Affiche TTC/main.js
@@ -1,22 +1,37 @@
 'use strict'
 
-document.write("<p>L'utilisateur saisit un montant HT, le résultat TTC s'affiche en HTML.</p>")
+// https://javascript.info/onload-ondomcontentloaded
+document.addEventListener('DOMContentLoaded', function () {
+    document.write("<p>L'utilisateur saisit un montant HT, le résultat TTC s'affiche en HTML.</p>")
 
-const TVA = 20.0
-var montantHT = prompt("Saisissez un montant:")
+    const TVA = 20
+    let montantHT = 0
+
+    // Attention ici un utilisateur peut entrer des lettres des chiffres negatifs
+    do {
+        montantHT = prompt("Saisissez un montant:")
+    } while (isNaN(montantHT) || montantHT < 0)
+
+    const result = calculerTTC(montantHT, TVA)
+
+    document.write("<p>Le taux de la TVA est de:" + TVA + "%" + "</p>")
+    document.write("<p>Le montant HT est:" + montantHT + "€" + "</p>")
+    document.write("<p>Le montant de la TVA est:" + result.montantTVA + "€" + "</p>")
+    document.write("<p>Le montant TTC est:" + result.totalTTC + "€" + "</p>")
+
+})
+
+// En mettant le calcul dans une fonction tu isoles la partie logique et peux donc la tester unitairement
+// https://blog.xebia.fr/2013/03/21/introduction-aux-tests-unitaires-en-javascript/
+function calculerTTC(montantHT, TVA) {
     montantHT = parseFloat(montantHT)
+    const montantTVA = (montantHT * TVA) / 100
+    const totalTTC = montantHT + montantTVA
 
-var montantTVA = (montantHT*TVA)/100
-    // montantTVA = parseFloat(montantTVA)   pas nécéssaire, c'est à la saisie par l'utilisateur qu'il faut indiquer le type (entier ou charactères)
-    // console.log(montantTVA)
-
-var totalTTC = (montantHT + montantTVA)
-    // totalTTC = parseFloat(totalTTC)
-
-document.write("<p>Le taux de la TVA est de:" + TVA + "%" + "</p>")
-document.write("<p>Le montant HT est:" + montantHT + "€" + "</p>")
-document.write("<p>Le montant de la TVA est:" + montantTVA + "€" + "</p>")
-document.write("<p>Le montant TTC est:" + totalTTC + "€" + "</p>")
-
+    return {
+        montantTVA,
+        totalTTC
+    }
+}
 
 
