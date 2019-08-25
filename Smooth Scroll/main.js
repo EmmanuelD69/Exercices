c9548d7c05926c6ea742e8c7b7589a3716805dbd
EmmanuelD69 <emmanueldurouge@yahoo.com>
2019-08-25 21:34:07 +0200

    part 2

--- Smooth Scroll/main.js
+++ Smooth Scroll/main.js
@@ -1,17 +1,44 @@
 function smoothScroll(targetToReach, durationAnim) {
-  var target = document.querySelection(targetToReach);
-  var targetPosition = target.getBoundingClientRect().top;
+  var targetToReach = document.querySelector(targetToReach);
+  console.log(targetToReach);
+  var targetPosition = targetToReach.getBoundingClientRect().top;
+  console.log(targetPosition);
   var startPosition = window.pageYOffset;
+  console.log(startPosition);
   var distance = targetPosition - startPosition;
+  console.log(distance);
   var startTime = null;
-}
 
-function animation(currentTime) {
-  if (startTime === null) {
-    startTime = currentTime;
+  function animation(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+      var timeElapsed = currentTime - startTime;
+      var run = ease(timeElapsed, startPosition, distance, durationAnim);
+      window.scrollTo(0, run);
+      if (timeElapsed < durationAnim) {
+        requestAnimationFrame(animation);
+      }
+    }
   }
 
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) {
+      return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+  }
   requestAnimationFrame(animation);
 }
 
-smoothScroll(".section2; 1000");
+var section1 = document.querySelector(".section1");
+var section2 = document.querySelector(".section2");
+
+section1.addEventListener("click", function() {
+  smoothScroll(".section2", 2000);
+});
+
+section2.addEventListener("click", function() {
+  smoothScroll(".section1", 2000);
+});
