100443dc41df0638852866e2251eb8c787aa5ae9
EmmanuelD69 <emmanueldurouge@yahoo.com>
2019-12-23 16:14:50 +0100

    ajout commentaires

--- Budget manager/app.js
+++ Budget manager/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/* fonction ayant pour but de demander le nom du budget */
 function budget() {
   let budgetName = window.prompt(
     "Quel nom voulez vous attribuer à votre Budget?"
@@ -7,10 +8,12 @@ function budget() {
   document.write("<h2>" + budgetName + "</h2>");
 }
 
+/* fonction ayant pour but de demander le montant du budget */
 function askBudget() {
   let budgetMontant = parseInt(
     window.prompt("A combien se monte votre budget?")
   );
+  /* si l'entrée ne correspond pas à un chiffre, elle est demandée à nouveau */
   if (isNaN(budgetMontant)) {
     alert("ce n'est pas un chiffre");
     return askBudget();
@@ -18,9 +21,11 @@ function askBudget() {
   document.write("<h2>" + budgetMontant + "€ </h2>");
 }
 
+/* c'est la fonction principale qui rassemble l'ensemble des fonction dans l'ordre prévu */
 function init() {
   budget();
   askBudget();
 }
 
+/* on lance la fonction init au chargement du DOM */
 init();
