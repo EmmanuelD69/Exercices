fef36541fc80a0a35248e11dd90f97d8f849f69c
EmmanuelD69 <emmanueldurouge@yahoo.com>
2019-12-24 14:25:43 +0100

    ajout fonction spent

--- Budget manager/app.js
+++ Budget manager/app.js
@@ -18,17 +18,39 @@ function askBudget() {
     alert("Votre saisie est incorrecte, veuillez saisir un chiffre.");
     return askBudget();
   } else if (budgetMontant <= 0) {
-  /* Et si la saisie est inférieure ou égale à 0, on demande une nouvelle saisie */
+    /* Et si la saisie est inférieure ou égale à 0, on demande une nouvelle saisie */
     alert("Votre budget ne peut pas être inférieur ou égal à 0");
     return askBudget();
   }
   document.write("<h2>" + budgetMontant + "€ </h2>");
 }
 
+/* fonction ayant pour but de demander le nom et le montant d'une dépense */
+function spent() {
+  let spentName = window.prompt(
+    "Quel nom voulez vous attribuer à cette dépense?"
+  );
+  document.write("<h2>" + spentName + "</h2>");
+
+  let spentMontant = parseInt(
+    window.prompt("Quel est le montant de cette dépense?")
+  );
+
+  /* Tant que la saisie n'est pas un chiffre ou est inférieure ou égale à 0, on demande une nouvelle saisie*/
+  while (isNaN(spentMontant) || spentMontant <= 0) {
+    alert("Votre saisie est incorrecte, veuillez saisir un chiffre.");
+    spentMontant = parseInt(
+      window.prompt("Quel est le montant de cette dépense?")
+    );
+  }
+  document.write("<h2>" + spentMontant + "€ </h2>");
+}
+
 /* c'est la fonction principale qui rassemble l'ensemble des fonction dans l'ordre prévu */
 function init() {
   budget();
   askBudget();
+  spent();
 }
 
 /* on lance la fonction init au chargement du DOM */
